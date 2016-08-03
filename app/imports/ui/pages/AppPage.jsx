2e8a60ebb47d0ab0e51447cc4a65c732cfba8dfe
Callum Macdonald <git@callum-macdonald.com>
2016-08-03 11:16:50 +0200

    Remove login form. Temporary hack.

--- app/imports/ui/pages/AppPage.jsx
+++ app/imports/ui/pages/AppPage.jsx
@@ -1,12 +1,10 @@
 import React from "react"
 import MyComponent from "../components/MyComponent"
-import { Accounts, STATES } from "meteor/std:accounts-semantic"
 
 export default class AppPage extends React.Component {
   render() {
     return (
       <div>
-        <Accounts.ui.LoginForm state={STATES.SIGN_IN}/>
         <h1>App Page</h1>
         <MyComponent/>
       </div>
